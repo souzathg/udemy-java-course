585977b696003960c4fd4210d9e225d809c027db
Thiago Costa de Souza <thgsouza@outlook.com>
2022-02-07 18:23:54 -0300

    solução com orientação à objetos: atributos e métodos // renaming

--- 1-OOP/src/application/TriangleNoOOP.java
+++ 1-OOP/src/application/TriangleNoOOP.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class NoOOP {
+public class TriangleNoOOP {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
--- 1-OOP/src/application/TriangleOOP.java
+++ 1-OOP/src/application/TriangleOOP.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 import entities.Triangle;
 
-public class OOP {
+public class TriangleOOP {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
