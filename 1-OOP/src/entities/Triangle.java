f467d4a2c418f5654bea0a3c9c91291e82b5b944
Thiago Costa de Souza <thgsouza@outlook.com>
2022-02-07 18:23:14 -0300

    solução com orientação à objetos: atributos e métodos // renaming

--- 1-OOP/src/entities/Triangle.java
+++ 1-OOP/src/entities/Triangle.java
@@ -8,11 +8,13 @@ public class Triangle {
      */
 
     // Atributos
+    // * Public indica que o atributo ou método pode ser usado em outros arquivos *
     public double a;
     public double b;
     public double c;
 
     // Métodos
+    // O double após o 'public' informa o tipo do dado que o método retorna
     public double area() {
         double p = (a + b + c) / 2.0;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
