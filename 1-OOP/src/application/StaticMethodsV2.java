0050e89bd1601ec086878a68ac199eaa87cbf1cc
Thiago Costa de Souza <thgsouza@outlook.com>
2022-02-07 22:10:58 -0300

    Static Methods: second version (w/ calculator class, but still w/o static methods)

---
+++ 1-OOP/src/application/StaticMethodsV2.java
@@ -0,0 +1,30 @@
+// Essa primeira versão da aplicação não terá métodos estáticos. Nas próximas versões já faremos uso desses recursos.
+
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+public class StaticMethodsV2 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double c = calc.circumference(radius);
+
+        double v = calc.volume(radius);
+
+        System.out.printf("Circumference: %.2f\n", c);
+        System.out.printf("Volume: %.2f\n", v);
+        System.out.printf("PI value: %.2f\n", calc.PI);
+
+        sc.close();
+    }
+}
