b69df44843332d943811d373636d388a20477dc9
Thiago Costa de Souza <thgsouza@outlook.com>
2022-02-10 20:42:35 -0300

    Arrays

---
+++ 3-Arrays/src/app/Program.java
@@ -0,0 +1,30 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        int n = sc.nextInt();
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+
+        double average = sum / n;
+
+        System.out.println("AVERAGE HEIGHT: " + average);
+
+        sc.close();
+    }
+}
