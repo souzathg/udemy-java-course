34b38ba4b69ddc3636228c0270449baa0e74b236
Thiago Costa de Souza <thgsouza@outlook.com>
2022-02-10 20:56:29 -0300

    Classes Arrays

--- 3-Arrays/src/app/Arrays.java
+++ 3-Arrays/src/app/Arrays.java
@@ -3,7 +3,7 @@ package app;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
