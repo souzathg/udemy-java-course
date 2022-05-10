ccc76e3dcb3fed4e6d6ff2aa41e093e715676385
Thiago Costa de Souza <thgsouza@outlook.com>
2022-05-10 11:21:10 -0300

    Herança

---
+++ 6-Herança/src/entities/SavingsAccount.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class SavingsAccount extends Account {
+
+    public Double interestRate;
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalance() {
+        balance += balance * interestRate;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+}
