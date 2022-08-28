7949da5b3ef78fb6cb0d5e6a22e7642fe7542c02
Thiago Costa de Souza <thgsouza@outlook.com>
2022-08-28 15:28:23 -0300

    Problem solved with the use of interfaces.

--- Interfaces/src/application/Program.java
+++ Interfaces/src/application/Program.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ProgramWithoutInterfaces {
+public class Program {
     public static void main(String[] args) throws ParseException {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
