52b29b1c2a3ab9f1f3be7faf361fa68be6e94e32
Thiago Costa de Souza <thgsouza@outlook.com>
2022-08-29 13:22:45 -0300

    Solution of proposed exercise.

--- 8-Interfaces/src/application/Exercise.java
+++ 8-Interfaces/src/application/Exercise.java
@@ -1,7 +1,46 @@
 package application;
 
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercise {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter contract data: ");
+        System.out.print("Number: ");
+        Integer contractNumber = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Date (dd/MM/yyyy): ");
+        Date contractStartDate = sdf.parse(sc.nextLine());
+        System.out.print("Contract value: ");
+        Double contractValue = sc.nextDouble();
+
+        Contract contract = new Contract(contractNumber, contractStartDate, contractValue);
+
+        System.out.print("Enter number of installments: ");
+        int installments = sc.nextInt();
+
+        ContractService cs = new ContractService(new PaypalService());
+
+        cs.processContract(contract, installments);
+
+        System.out.println("Installments:");
+        for (Installment it : contract.getInstallments()) {
+            System.out.println(it);
+        }
+
+        sc.close();
     }
 }
---
+++ 8-Interfaces/src/model/entities/Installment.java
@@ -0,0 +1,24 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Installment {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private Date dueDate;
+    private Double amount;
+
+    public Installment() {
+    }
+
+    public Installment(Date dueDate, Double amount) {
+        this.dueDate = dueDate;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return sdf.format(dueDate) + " - " + String.format("%.2f", amount);
+    }
+}
