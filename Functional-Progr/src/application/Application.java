df994c8e89aaf0e9e8dc1a02bc8de80986c585e2
Thiago Costa de Souza <thgsouza@outlook.com>
2022-11-23 18:42:57 -0300

    Comparator and Lambda Expressions

---
+++ Functional-Progr/src/application/Application.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        Comparator<Product> comp = (p1, p2) -> p1.getName().compareToIgnoreCase(p2.getName());
+
+        list.sort(comp);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
